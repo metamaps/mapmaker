c3725e32b2bfcfe4b83d5432e742056153ba9afc
Connor Turland <connorturland@gmail.com>
2014-12-31 00:15:57 -0500

    changed folder structure. started building out view tree

---
+++ ModulesToPort.js
@@ -0,0 +1,44 @@
+Metamaps.Realtime
+
+Metamaps.Listeners
+
+Metamaps.Synapse
+
+Metamaps.Topic
+
+Metamaps.Map
+
+Metamaps.Filter
+
+Metamaps.Control
+
+Metamaps.Selected
+
+Metamaps.Visualize
+
+Metamaps.JIT
+
+Metamaps.Settings -> defaultConfig
+
+Metamaps.Create
+
+
+mapView
+	infoBox (mapInfoBoxView)
+	topicCards
+		[] topicCardView (needs template?)
+	synapseCards
+		[] synapseCardView (needs template?)
+	filter (filterView) 
+	junto (juntoView)
+	newTopic (newTopicView)
+		metacodeSetSwitch (metacodeSetSwitchView)
+	newSynapse (newSynapseView)
+
+	topics
+		[] topicMappingView
+	synapses
+		[] synapseMappingView
+
+
+
