1fcda13d3941a7e7488082ece9c175e16ecac2a9
Devin Howard <devin@callysto.com>
2015-02-24 12:29:38 -0500

    add easyrtc library, a bunch of code from the tutorial to serve folder, and some symlinks. Also git ignore nohup.out and npm-debug.log

---
+++ serve/mylogic.js
@@ -0,0 +1,42 @@
+function my_init() {
+  easyrtc.setRoomOccupantListener( roomListener);
+  easyrtc.easyApp("Company_Chat_Line", "self", ["caller"],
+    function(myId) {
+      console.log("My easyrtcid is " + myId);
+    }
+  );
+}
+
+function roomListener(roomName, otherPeers) {
+    var otherClientDiv = document.getElementById('otherClients');
+    while (otherClientDiv.hasChildNodes()) {
+        otherClientDiv.removeChild(otherClientDiv.lastChild);
+    }
+    for(var i in otherPeers) {
+        var button = document.createElement('button');
+        button.onclick = function(easyrtcid) {
+            return function() {
+                performCall(easyrtcid);
+            }
+        }(i);
+
+        label = document.createTextNode(i);
+        button.appendChild(label);
+        otherClientDiv.appendChild(button);
+    }
+}
+
+function performCall(easyrtcid) {
+    easyrtc.call(
+       easyrtcid,
+       function(easyrtcid) { console.log("completed call to " + easyrtcid);
+       function(errorMessage) { console.log("err:" + errorMessage);
+       function(accepted, bywho) {
+          console.log((accepted?"accepted":"rejected")+ " by " + bywho);
+       }
+    );
+}
+
+jQuery(document).ready(function() {
+  my_init();
+});
