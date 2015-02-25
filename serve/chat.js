6765eea0fe82fde720be9772e743571f4f2a6c55
Devin Howard <devin@callysto.com>
2015-02-25 14:54:38 -0500

    add broken text chat feature, that sort of works. TODO: change this to a broadcast instead of a direct message

--- serve/chat.js
+++ serve/chat.js
@@ -6,18 +6,18 @@ function setUpButton(op) {
   if (op === 'open') {
     $('#easyRTCWrapper').append('<button id="videoChatButton">Open Video Chat</button>');
     $('#videoChatButton').click(function() {
-      openChat(0, 'devvmh');
+      openVideo(0, 'devvmh');
     });
   } else if (op === 'close') {
     $('#videoChatButton').html('Close Video Chat');
     $('#videoChatButton').unbind('click');
     $('#videoChatButton').click(function() {
-      closeChat();
+      closeVideo();
     });
   }
 }//setUpButton
 
-function openChat(mapid, userid) {
+function openVideo(mapid, userid) {
   setUpButton('close');
   createHTMLObjects();
   easyrtc.setSocketUrl("//localhost:5002");
@@ -29,7 +29,14 @@ function openChat(mapid, userid) {
   );
 }
 
-function closeChat() {
+function connectChat(mapid, userid) {
+  easyrtc.setSocketUrl("//localhost:5002");
+  easyrtc.setPeerListener(addToConversation);
+  easyrtc.setRoomOccupantListener(convertListToButtons);
+  easyrtc.connect("easyrtc.instantMessaging_mapid_" + mapid, loginSuccess, loginFailure);
+}
+
+function closeVideo() {
   easyrtc.disconnect();
   destroyHTMLObjects();
   setUpButton('open');
@@ -75,4 +82,56 @@ function performCall(easyrtcid) {
           console.log((accepted?"accepted":"rejected")+ " by " + bywho);
        }
     );
+}
+
+var selfEasyrtcid = "";
+function addToConversation(who, msgType, content) {
+  // Escape html special characters, then add linefeeds.
+  content = content.replace(/&/g,"&amp;").replace(/</g,"&lt;").replace(/>/g,"&gt;");
+  content = content.replace(/\n/g, "<br />");
+  document.getElementById("conversation").innerHTML +=
+  "<b>" + who + ":</b>&nbsp;" + content + "<br />";
+}
+ 
+function convertListToButtons (roomName, occupants, isPrimary) {
+  var otherClientDiv = document.getElementById("otherClients");
+  while (otherClientDiv.hasChildNodes()) {
+    otherClientDiv.removeChild(otherClientDiv.lastChild);
+  }
+ 
+  for(var easyrtcid in occupants) {
+    var button = document.createElement("button");
+    button.onclick = function(easyrtcid) {
+      return function() {
+        sendStuffWS(easyrtcid);
+      };
+    }(easyrtcid);
+    var label = document.createTextNode("Send to " + easyrtc.idToName(easyrtcid));
+    button.appendChild(label);
+ 
+    otherClientDiv.appendChild(button);
+  }
+  if( !otherClientDiv.hasChildNodes() ) {
+    otherClientDiv.innerHTML = "<em>Nobody else logged in to talk to...</em>";
+  }
+}
+ 
+function sendStuffWS(otherEasyrtcid) {
+  var text = document.getElementById("sendMessageText").value;
+  if(text.replace(/\s/g, "").length === 0) { // Don"t send just whitespace
+    return;
+  }
+ 
+  easyrtc.sendDataWS(otherEasyrtcid, "message",  text);
+  addToConversation("Me", "message", text);
+  document.getElementById("sendMessageText").value = "";
+}
+ 
+function loginSuccess(easyrtcid) {
+  selfEasyrtcid = easyrtcid;
+  document.getElementById("iam").innerHTML = "I am " + easyrtcid;
+}
+ 
+function loginFailure(errorCode, message) {
+  easyrtc.showError(errorCode, message);
 }
